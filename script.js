bf57498e99511e6634545e021c487cb63e7680db
MihasTJ <michal.janiol00@gmail.com>
2024-03-13 18:18:34 +0100

    12line to rework (not finding class flex)

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ if (innerWidth <= 678){
 const desc = document.querySelector('.header__desc');
 const img = document.querySelector('.header__img');
 const username = document.querySelector('.main__contact-username');
-
+const main = document.querySelector('.main');
 
 img.parentNode.insertBefore(desc, img.nextSibling);
+img.parentNode.insertBefore(username, img.nextSibling);
+
+if(main.className == "flex") main.classList.remove("flex");
