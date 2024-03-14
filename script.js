4d8804c1291493a64a5496bb5e1be0ec946d28e5
MihasTJ <michal.janiol00@gmail.com>
2024-03-14 22:58:25 +0100

    fixed bugs, changed p70 to p30

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const desc = document.querySelector('.header__desc');
 const img = document.querySelector('.header__img');
 const username = document.querySelector('.main__contact-username');
 const main = document.querySelector('.main');
+const padding70 = document.querySelectorAll('.py-70, .px-70');
+
 
 img.parentNode.insertBefore(desc, img.nextSibling);
 img.parentNode.insertBefore(username, img.nextSibling);
-
-if(main.className == "flex") main.classList.remove("flex");
+/*if(main.className.contains("flex")) */
+main.classList.remove("flex");
+padding70.forEach(element =>{
+    element.classList.remove('py-70', 'px-70');
+    element.classList.add('px-30', 'px-30');
+})
