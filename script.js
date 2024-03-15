d7b53422fb78c449d98d63329c1667720c5f22b2
MihasTJ <michal.janiol00@gmail.com>
2024-03-15 20:16:36 +0100

    RWD, fixed script.js, added class

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-if (innerWidth <= 678){
-    console.log('test');
+/*moving some div to moblie version, and changing classlists */
+if (innerWidth <= 767){
+    const desc = document.querySelector('.header__desc');
+    const img = document.querySelector('.header__img');
+    const username = document.querySelector('.main__contact-username');
+    const main = document.querySelector('.main');
+    const padding70 = document.querySelectorAll('.py-70, .px-70');
+    
+    main.classList.remove("flex");
+    img.parentNode.insertBefore(desc, img.nextSibling);
+    img.parentNode.insertBefore(username, img.nextSibling);
+    desc.classList.add('py-30');
+    padding70.forEach(element =>{
+        element.classList.remove('py-70', 'px-70');
+        element.classList.add('px-30', 'px-30');
+    })
 }
-const desc = document.querySelector('.header__desc');
-const img = document.querySelector('.header__img');
-const username = document.querySelector('.main__contact-username');
-const main = document.querySelector('.main');
-const padding70 = document.querySelectorAll('.py-70, .px-70');
-
-
-img.parentNode.insertBefore(desc, img.nextSibling);
-img.parentNode.insertBefore(username, img.nextSibling);
-/*if(main.className.contains("flex")) */
-main.classList.remove("flex");
-padding70.forEach(element =>{
-    element.classList.remove('py-70', 'px-70');
-    element.classList.add('px-30', 'px-30');
-})
