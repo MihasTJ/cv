ff02a9df63857084a2de7d7503537a0acea38bf3
MihasTJ <michal.janiol00@gmail.com>
2024-03-16 19:47:46 +0100

    darkmode

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (innerWidth <= 767){
     const username = document.querySelector('.main__contact-username');
     const main = document.querySelector('.main');
     const padding70 = document.querySelectorAll('.py-70, .px-70');
-    
+
     main.classList.remove("flex");
     img.parentNode.insertBefore(desc, img.nextSibling);
     img.parentNode.insertBefore(username, img.nextSibling);
@@ -14,4 +14,22 @@ if (innerWidth <= 767){
         element.classList.remove('py-70', 'px-70');
         element.classList.add('px-30', 'px-30');
     })
+}
+/* darkmode */
+const darkbtn = document.getElementById("darkbtn");
+
+const toggleDarkMode = () =>{
+    document.body.classList.toggle("darkmode");
+    // saving value in localstorage
+    localStorage.setItem('darkmode', document.body.classList.contains('darkmode')?'darkON':'darkOFF');
+};
+
+//listener on darkmodebtn
+darkbtn.addEventListener("click", toggleDarkMode);
+//reading value from localStorage
+const darkmodeS = localStorage['darkmode']
+
+//checking value darkmode, 
+if (darkmodeS ==='darkON'){
+    toggleDarkMode();
 }
